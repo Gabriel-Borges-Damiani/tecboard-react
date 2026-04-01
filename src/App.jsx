b62270f74ab2436ae9ad0f7d9551386c9dbfb0fe
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-04-01 08:18:46 -0300

    Cria componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
-import './App.css'
-import { FormularioDeEvento } from './componentes/FormularioDeEvento'
-import { Tema } from './componentes/Tema'
-
+import "./App.css";
+import { Banner } from "./componentes/Banner";
+import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 function App() {
-
   const temas = [
     {
       id: 1,
-      nome: 'front-end'
+      nome: "front-end",
     },
     {
       id: 2,
-      nome: 'back-end'
+      nome: "back-end",
     },
     {
       id: 3,
-      nome: 'devops'
+      nome: "devops",
     },
     {
       id: 4,
-      nome: 'inteligência artificial`'
+      nome: "inteligência artificial`",
     },
     {
       id: 5,
-      nome: 'data science'
+      nome: "data science",
     },
     {
       id: 6,
-      nome: 'cloud'
+      nome: "cloud",
     },
-  ]
-  
+  ];
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
       <section>
         <Tema tema={temas[1]} />
@@ -57,7 +54,7 @@ function App() {
         <Tema tema={temas[5]} />
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
