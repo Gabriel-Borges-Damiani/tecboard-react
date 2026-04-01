50c74e48f696bb55ffd00205e7379b127353233f
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-04-01 12:40:52 -0300

    Adiciona atributos a ListaSuspensa

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -1,44 +1,41 @@
-import './formulario-de-evento-estilos.css'
+import "./formulario-de-evento-estilos.css";
 import { CampoDeEntrada } from "../CampoDeEntrada";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
 import { TituloFormulario } from "../TituloFormulario";
-import { ListaSuspensa } from '../ListaSuspensa';
-import { Botao } from '../Botao';
+import { ListaSuspensa } from "../ListaSuspensa";
+import { Botao } from "../Botao";
 
-
-export function FormularioDeEvento(){
-
+export function FormularioDeEvento() {
   return (
-    <form className='form-evento'>
-      <TituloFormulario>
-        Preencha para criar um evento:
-      </TituloFormulario>
-      <div className='campos'>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">
-          Qual o nome do evento?
-        </Label>
-        <CampoDeEntrada className="text" id='nome' placeholder='Summer dev hits'></CampoDeEntrada>
-      </CampoDeFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="dataEvento">
-          Data do evento
-        </Label>
-        <CampoDeEntrada type="date" id='dataEvento' placeholder='Summer dev hits' name='dataEvento'></CampoDeEntrada>
-      </CampoDeFormulario>
-      <CampoDeFormulario>
-            <Label htmlFor="dataEvento">
-              Tema do evento
-            </Label>
-            <ListaSuspensa />
-          </CampoDeFormulario>
-        </div>
-        <div className='acoes'>
-          <Botao>
-            Criar evento
-          </Botao>
+    <form className="form-evento">
+      <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nome">Qual o nome do evento?</Label>
+          <CampoDeEntrada
+            className="text"
+            id="nome"
+            placeholder="Summer dev hits"
+          ></CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do evento</Label>
+          <CampoDeEntrada
+            type="date"
+            id="dataEvento"
+            placeholder="Summer dev hits"
+            name="dataEvento"
+          ></CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Tema do evento</Label>
+          <ListaSuspensa id="tema" name="tema" />
+        </CampoDeFormulario>
+      </div>
+      <div className="acoes">
+        <Botao>Criar evento</Botao>
       </div>
     </form>
-  )
+  );
 }
